8d1dc651150c9cd79965c48c90473b9d4a9fefe6
VivienChen <chenxin4869@gmail.com>
2018-09-20 18:49:48 -0400

    keras epoch = 10 batch_size = 32 first two layer init = random_normal optimizer = adam(lr = 0.001) with standardization

--- nyc-taxi-fare-prediction-keras.py
+++ nyc-taxi-fare-prediction-keras.py
@@ -81,7 +81,7 @@ model.add(Dense(8, init = 'uniform', activation = 'tanh'))
 model.add(Dense(4, init = 'uniform', activation = 'tanh'))
 model.add(Dense(1, init = 'uniform'))
 
-adam = optimizers.Adam(lr=0.0001)
+adam = optimizers.Adam(lr=0.001)
 model.compile(loss = 'mean_squared_error', optimizer = adam)
 
 model.fit(train_X, train_y, nb_epoch = 2, batch_size = 32)
