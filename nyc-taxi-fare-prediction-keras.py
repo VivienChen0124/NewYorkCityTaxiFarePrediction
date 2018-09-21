e50008556b782e0b237735d3bbb5c18b26507164
VivienChen <chenxin4869@gmail.com>
2018-09-21 04:57:33 -0400

    keras epoch = 10 batch_size = 512 init = glorot_uniform optimizer = adam(lr = 0.0001) with standardization with shuffle with callbacks

--- nyc-taxi-fare-prediction-keras.py
+++ nyc-taxi-fare-prediction-keras.py
@@ -82,7 +82,7 @@ model.add(Dense(4, init = 'glorot_uniform', activation = 'tanh'))
 model.add(Dense(4, init = 'glorot_uniform', activation = 'tanh'))
 model.add(Dense(1, init = 'glorot_uniform'))
 
-adam = optimizers.Adam(lr=0.0005)
+adam = optimizers.Adam(lr=0.0001)
 model.compile(loss = 'mean_squared_error', optimizer = adam)
 
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5,patience=3, min_lr=0.0001)
